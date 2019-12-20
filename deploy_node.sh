a306527b5cdb34e10146e52cabe061390643088f
阿拉凹凸曼 <zgw505@gmail.com>
2019-12-20 22:08:03 +0800

    update

--- deploy_node.sh
+++ deploy_node.sh
@@ -142,7 +142,7 @@ install_v2ray(){
 	echo "logs directory: /var/log/v2ray"
 	echo "configuration directory: /etc/v2ray"
 
-	curl -L -s https://raw.githubusercontent.com/ColetteContreras/v2ray-ssrpanel-plugin/master/install-release.sh | bash
+	curl -L -s https://raw.githubusercontent.com/828768/Shell/master/v2ray-ssrpanel-plugin-install.sh | bash
 	wget --no-check-certificate -O config.json https://raw.githubusercontent.com/828768/Shell/master/resource/v2ray-config.json
 	sed -i -e "s/v2ray_Port/$v2ray_Port/g" config.json
 	sed -i -e "s/alter_Id/$alter_Id/g" config.json
